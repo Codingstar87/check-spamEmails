5114d025f7f6b7b62935247c9aa873993694dc87
Codingstar87 <abhijitdatta54321@gmail.com>
2025-01-15 18:28:19 +0530

    fix frontend error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+   
     server: {
         proxy: {
             '/auth': {
-                target: process.env.NODE_ENV === 'production' 
-                    ? 'https://check-spamemails-2.onrender.com' 
-                    : 'http://127.0.0.1:8000', 
+                target:'https://check-spamemails-2.onrender.com' , 
+                   
                 changeOrigin: true,
             },
         },
