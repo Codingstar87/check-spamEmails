fdd62ce6772664030b8e5be98275ffc6e7f3b176
Codingstar87 <abhijitdatta54321@gmail.com>
2025-01-16 10:59:16 +0530

    add frontend local host

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://check-spem-email.netlify.app"],
+    allow_origins=["http://localhost:3000", "https://check-spem-email.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
